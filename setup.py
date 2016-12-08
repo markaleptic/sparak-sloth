113702fd69dd499bc02b58e102be3aab561417bf
Mark Allred <markjallred@gmail.com>
2016-12-08 14:51:47 -0700

    Initial implementation of setup file. Current build errors are around tkinter and dll files.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import cx_Freeze
+import sys
+import os
+
+#os.environ['TCL_LIBRARY'] = "C:\\Users\\mallred\\AppData\\Local\\Continuum\\Anaconda3\\tcl\\tcl8.6"
+#os.environ['TK_LIBRARY']  = "C:\\Users\\mallred\\AppData\\Local\\Continuum\\Anaconda3\\tcl\\tk8.6"
+
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+executables = [cx_Freeze.Executable("Sparak_Sloth.py", base=base, icon='slothicon.ico')]
+
+build_packages = ["pyautogui","tkinter"]
+include_files = ["slothicon.ico", "slothBackground.png", "delete_sign.png", "plus_sign.png",
+                 "C:\\Users\\mallred\\AppData\\Local\\Continuum\\Anaconda3\\DLLs\\tcl86t.dll",
+                 "C:\\Users\\mallred\\AppData\\Local\\Continuum\\Anaconda3\\DLLs\\tk86t.dll"
+                 ]
+
+cx_Freeze.setup(
+    name = "Sparak Accounting Sloth",
+    options = {"build_exe": {"packages"     :   build_packages, 
+                             "include_files":   include_files}},
+    version = "0.01",
+    description = "Automated Sparak Bookkeeping Entries",
+    executables = executables
+    )
